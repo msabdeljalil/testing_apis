7732cab32e05d91a3fe397e5aff3772be2720063
Mohammad Abdeljalil <mabdeljalil@enova.com>
2016-05-08 09:46:29 -0500

    Initial API Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestingApis::Application.config.secret_key_base = 'f4aba638031d23767eb90d2b55a207872b40b912a04a3925dd93a8fd27294ceaf761359b58f7faee2a5cda1801700b7f7343a28b9da7d70718077a896edfbe07'
