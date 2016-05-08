21fe73b4ce7c1e9b45699e8c6c015e53ec47930b
Mohammad Abdeljalil <mabdeljalil@enova.com>
2016-05-08 10:04:21 -0500

    Making it API only

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ TestingApis::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # This renders debugging information, preserving the response format
+  # -- The below is actually the default when "config.api_only = true" in application.rb
+  config.debug_exception_response_format = :api
 end
